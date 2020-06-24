e92745207454dcfd8d9f8ce7aeb38fad95fbb044
Afonso Santos <afonso.faisca1@gmail.com>
2020-06-24 18:15:29 +0100

    Adicionadas mais funções matemáticas

--- main.py
+++ main.py
@@ -1,14 +1,53 @@
 """Módulo principal"""
 
 
-def soma(num_1, num_2):
+def soma(num_1: int, num_2: int):
     """Calcula a soma de dois números
 
     Args:
-        num_1 (integer): O primeiro número
-        num_2 (integer): O segundo número
+        num_1 (int): O primeiro número
+        num_2 (int): O segundo número
 
     Returns:
-        integer: A soma dos dois números
+        int: A soma dos dois números
     """
     return num_1 + num_2
+
+
+def subtracao(num_1: int, num_2: int):
+    """Calcula a subtração de dois números
+
+    Args:
+        num_1 (int): O primeiro número
+        num_2 (int): O segundo número
+
+    Returns:
+        int: A subtração dos dois números
+    """
+    return num_1 - num_2
+
+
+def multiplicacao(num_1: int, num_2: int):
+    """Calcula a multiplicação de dois números
+
+    Args:
+        num_1 (int): O primeiro número
+        num_2 (int): O segundo número
+
+    Returns:
+        int: A multiplicação dos dois números
+    """
+    return num_1 * num_2
+
+
+def divisao(num_1: int, num_2: int):
+    """Calcula a divisão de dois números
+
+    Args:
+        num_1 (int): O primeiro número
+        num_2 (int): O segundo número
+
+    Returns:
+        int: A divisão dos dois números
+    """
+    return num_1 / num_2
