e9b7763b81a7d950473937d12ef12455975ce73d
Afonso Santos <afonso.faisca1@gmail.com>
2020-06-24 08:44:53 +0100

    Adicionadas docstrings

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+"""Módulo principal"""
+
+
 def soma(num_1, num_2):
+    """Calcula a soma de dois números
+
+    Args:
+        num_1 (integer): O primeiro número
+        num_2 (integer): O segundo número
+
+    Returns:
+        integer: A soma dos dois números
+    """
     return num_1 + num_2
